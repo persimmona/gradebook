2d408f3bb1360e907b937c95b8da3add2fa3b7cf
hurma <ad286766@gmail.com>
2020-01-14 13:46:52 +0200

    Update register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title','Авторизуватися — Журнал Оцінок')
+@section('title','Зареєструватися — Журнал Оцінок')
 
 @section('content')
     <form class="login-form" action="" method="">
